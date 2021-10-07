9bee4df95d17f2e2456e148d3f451985bdec9f55
Crowdbotics <team@crowdbotics.com>
2021-10-07 07:45:46 +0000

    Update models

--- pms_31190/wsgi.py
+++ pms_31190/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pms_31190.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pms_31190.settings")
 
 application = get_wsgi_application()
